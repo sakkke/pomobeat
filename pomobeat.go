3803c80a067318295c10c441f1d5065adc56f247
sakkke <w32w64@gmail.com>
2023-08-16 00:26:34 +0900

    :sparkles: implement hello world

--- pomobeat.go
+++ pomobeat.go
@@ -1,9 +1,14 @@
 package main
 
 import (
-	"fmt"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
-	fmt.Println("ok")
+	a := app.New()
+	w := a.NewWindow("Hello World")
+
+	w.SetContent(widget.NewLabel("Hello World!"))
+	w.ShowAndRun()
 }
